7796b2933191d5dfb554e756ec8eb987e1a522f0
ryanmarshall050 <ryanmarshall050@gmail.com>
2025-03-12 13:32:38 -0400

    added backend stuff

---
+++ backEndCalls.js
@@ -0,0 +1,15 @@
+function getBooks(searchTerm){
+
+}
+
+function addBook(book, username, password){
+
+}
+
+function rmBook(book, username, password){
+
+}
+
+function listBooks(book, username, password){
+
+}
