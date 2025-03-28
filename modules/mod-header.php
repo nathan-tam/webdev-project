38d1f5a5b1c0b849b5497a7ba876e43b63e1f745
JLatkowcer <jon.email@gmail.com>
2025-03-28 19:24:42 -0400

    Signout button in header improved, signout doesn't show up if not logged in

--- modules/mod-header.php
+++ modules/mod-header.php
@@ -1,4 +1,15 @@
+<?php //Written by Jonathan Latkowcer?>
+
 <header class="pageHeader">
-        <a href="login.php"><button id="signOut">Sign Out</button></a>
+        
+    <?php 
+    // Only show the signout button if the user is logged in (mostly affects AboutUs page)
+    if (isset($_SESSION['username'])){ ?>
+        <form action="scripts/logoutScript.php" method="post">
+            <button id="signOut" type="submit">Sign Out</button>
+        </form>
+       <?php }  ?>
+        
+    <!-- Sends the user back to the index page -- if they're signed in, they'll go back to their bookshelf instead  -->
         <h1 id="bookedLogo"><a href="index.php">booked</a></h1>
 </header>
