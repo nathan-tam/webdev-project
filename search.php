d66b72b9b08fd89e958c1fe29e1e5ab5a488dc89
mcotter001 <cott0075@algonquinlive.com>
2025-03-27 23:06:41 -0400

    Adding CSS and html for the book containers in bookshelf and search

--- search.php
+++ search.php
@@ -15,6 +15,21 @@
                 <input id="searchBarItem" type="text" placeholder="Enter book title...">
                 <button id="searchButton" type="button">Search &#x1F50D;</button>
             </div>
+            <div class="bookContainer">
+                <div class="bookItem">
+                    <img class="bookCover" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                    <div class="bookContents">
+                        <p class="bookTitle">Title</p>
+                        <p class="bookDescription">Author</p>
+                        <p class="bookDescription">Description</p>
+                    </div>
+                    <div class="AddBookContainer">
+                        <button id="AddBookButton" type="button">+ Add Book</button>
+                    </div>
+                </div>
+            </div>
         </main>
+
+
     </body>
 </html>
