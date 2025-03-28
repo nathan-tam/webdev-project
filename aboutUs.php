927728b49e4ab789fe4b9d685aac13493d73df93
mcotter001 <cott0075@algonquinlive.com>
2025-03-27 23:32:34 -0400

    Added an about us page, fixed CSS, and began to contemplate how mobile gets added

---
+++ aboutUs.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php session_start();?>
+<html id="background" lang="en">
+    <head>
+        <link rel="stylesheet" href="main-stylesheet.css">
+        <title>Booked</title>
+    </head>
+
+    <header class="pageHeader">
+        <a href="login.html"><button id="signOut">Sign Out</button></a>
+        <h1 id="bookedLogo">booked</h1>
+    </header>
+
+    <body>
+
+        <main>
+            <div id="aboutUsContainer">
+                    <div class="aboutUsItem">
+                        <img class="aboutUsImage" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                        <div class="bookContents">
+                            <p class="bookTitle">Title</p>
+                            <p class="bookDescription">Author</p>
+                            <p class="bookDescription">Description</p>
+                        </div>
+                    </div>
+                </div>
+        </main>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -11,6 +11,8 @@
             <div id="buttonContainer">
                 <a href="login.php"><button class="welcomeButton">Login</button></a>
                 <a href="register.php"><button class="welcomeButton">Sign Up</button></a>
+                <a href="aboutUs.php"><button class="welcomeButton">About Us</button></a>
+
             </div>
         </main>
     </body>
