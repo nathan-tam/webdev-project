ff18d66b433cbeccb3391a458e7740b2a5487cea
JLatkowcer <jon.email@gmail.com>
2025-03-29 17:20:32 -0400

    Added some images to AboutUs.

--- aboutUs.php
+++ aboutUs.php
@@ -15,9 +15,10 @@
         <!-- Header module on all pages (booked logo and rightside link) -->
         <?php include('modules/mod-header.php'); ?>
         <main>
+        <!-- AboutUs uses some of the same CSS as the book listings -->
             <div id="aboutUsContainer">
                 <div class="aboutUsItem">
-                    <img class="aboutUsImage" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                    <img class="aboutUsImage" src="images/mikaela-small.png" alt="A headshot of a young woman.">
                     <div class="bookContents">
                         <p class="bookTitle">Mikaela Cotter</p>
                         <p class="bookDescription">Front End Designer</p><br>
@@ -25,15 +26,15 @@
                     </div>
                 </div>
                 <div class="aboutUsItem">
-                    <img class="aboutUsImage" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                    <img class="aboutUsImage" src="images/latk0004-headshot-small.jpg" alt="A headshot of a young man.">
                     <div class="bookContents">
                         <p class="bookTitle">Jonathan Latkowcer</p>
                         <p class="bookDescription">Team Leader</p><br>
-                        <p class="bookDescription">Those who do can, code. Those who can't, lead.</p>
+                        <p class="bookDescription">Those who do can, build.<br><br>Those who can't, lead.</p>
                     </div>
                 </div>
                 <div class="aboutUsItem">
-                    <img class="aboutUsImage" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                    <img class="aboutUsImage" src="images/Ryan-AI.jpg" alt="A caricature of a young man with technology-related cartoon images around him.">
                     <div class="bookContents">
                         <p class="bookTitle">Ryan Marshall</p>
                         <p class="bookDescription">Back End Developer</p><br>
@@ -41,7 +42,7 @@
                     </div>
                 </div>
                 <div class="aboutUsItem">
-                    <img class="aboutUsImage" src="bookNoCover.png" alt="Purple book cover with dark purple lines to indicate a book with no cover">
+                    <img class="aboutUsImage" src="images/nathan-small.png" alt="A headshot of a young man while he works on a computer.">
                     <div class="bookContents">
                         <p class="bookTitle">Nathan Tam</p>
                         <p class="bookDescription">Front End Developer</p><br>
