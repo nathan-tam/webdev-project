cc94b790d1c7fd94d94875cc92056e633f10acf2
JLatkowcer <jon.email@gmail.com>
2025-03-28 20:39:38 -0400

    loginscript error fixed, more DML SQL

--- db _files/DML.sql
+++ db _files/DML.sql
@@ -4,3 +4,5 @@ INSERT INTO users(username, passwordHash) VALUES ('jonathan', '$2y$10$SvwZrUoVtZ
 INSERT INTO users(username, passwordHash) VALUES ('nathan', '$2y$10$SvwZrUoVtZKD3r6SGnJUjOq0oY9t9a5ca91Tf/EPp/62Bq7L3H.dG');
 
 INSERT INTO books(ISBN, title, author, description, coverImage) VALUES ('9780134190440', 'Java: The Complete Reference', 'Herbert Schildt', 'The Definitive Guide to Java Programming Language', 'images/bookNoCover.png');
+
+INSERT INTO usersbooks(userID, ISBN) VALUES (1, '9780134190440');
