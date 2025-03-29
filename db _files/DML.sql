d56b937e546f71b47930878ab5bf5c0e94088f03
JLatkowcer <jon.email@gmail.com>
2025-03-28 20:34:25 -0400

    fixing database files to have a DDL and DML, testing index.php

---
+++ db _files/DML.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO users(username, passwordHash) VALUES ('jonathan', '$2y$10$SvwZrUoVtZKD3r6SGnJUjOq0oY9t9a5ca91Tf/EPp/62Bq7L3H.dG');
+INSERT INTO users(username, passwordHash) VALUES ('nathan', '$2y$10$SvwZrUoVtZKD3r6SGnJUjOq0oY9t9a5ca91Tf/EPp/62Bq7L3H.dG');
+
+INSERT INTO books(ISBN, title, author, description, coverImage) VALUES ('9780134190440', 'Java: The Complete Reference', 'Herbert Schildt', 'The Definitive Guide to Java Programming Language', 'images/bookNoCover.png');
