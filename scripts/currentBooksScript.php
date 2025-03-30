81cc057f4494428952237bb871b0c4cdb498691f
mcotter001 <cott0075@algonquinlive.com>
2025-03-30 00:05:25 -0400

    Fixing currentbooksScript.php script and adjusting css

--- scripts/currentBooksScript.php
+++ scripts/currentBooksScript.php
@@ -84,20 +84,19 @@
   //   Using htmlspecialchars to prevent html mischief
     foreach ($books as $book) {
         echo '<div class="bookItem">';
-        echo '<div class="bookOverview"';
+        echo '<div class="bookOverview">';
         echo '<img class="bookCover" src="' . htmlspecialchars($book["coverImage"]) . '" alt="Book cover">';
         echo '<div class="bookContents">';
         echo '<p class="bookTitle">' . htmlspecialchars($book["title"]) . '</p>';
         echo '<p class="bookAuthor">' . htmlspecialchars($book["author"]) . '</p>';
         echo '<p class="bookYear">' . htmlspecialchars($book["year"]) . '</p>';
-        // Hidden form so that the Remove Book button can communicate which book it belongs to
-     //   echo '<form method="POST" action="scripts/removeBookScript.php">';
-     //   echo '<input type="hidden" name="isbntoremove" value="' . htmlspecialchars($book["ISBN"]) . '">';   
-    //    echo '<button type="submit">Remove book</button>';
-         echo '<button type="button" class="removeButton" onclick="showModal(\'' . htmlspecialchars($book["ISBN"]) . '\')">Remove Book</button>';
-
+        echo '</div>';
+        echo '<div class="bookContents">';
+        echo '<form>';
+        echo '<button type="button" class="removeButton" onclick="showModal(\'' . htmlspecialchars($book["ISBN"]) . '\')">Remove Book</button>';
         echo '</form>';
         echo '</div>';
+        
         echo '</div>';
         echo '</div>';
     }
