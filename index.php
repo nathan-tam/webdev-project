b1e03f02575913f57aceedcacdcb385681c869e0
jjorge146 <egrojj146@gmail.com>
2025-03-29 22:11:18 -0400

    fixed div change, whoops

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php session_start();?>
 
-<?php
+<a?php
 
     // Use the variable below for testing.
     //$_SESSION['username'] = 'jonathan'; 
@@ -23,9 +23,9 @@
         <main>
             <h1 id="welcomeBanner">welcome to booked!</h1>
             <div id="buttonContainer">
-                <div href="login.php"><button class="welcomeButton">Login</button></div>
-                <div href="register.php"><button class="welcomeButton">Sign Up</button></div>
-                <div href="aboutUs.php"><button class="welcomeButton">About Us</button></div>
+                <a href="login.php"><button class="welcomeButton">Login</button></a>
+                <a href="register.php"><button class="welcomeButton">Sign Up</button></a>
+                <a href="aboutUs.php"><button class="welcomeButton">About Us</button></a>
             </div>
             <div>
                 <?php if (isset($_SESSION["dberror"])){
