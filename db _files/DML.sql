f024632557179076f747fbcc0e300b5d5b17defb
JLatkowcer <jon.email@gmail.com>
2025-03-29 23:24:01 -0400

    DML adjusted for new db columns

--- db _files/DML.sql
+++ db _files/DML.sql
@@ -5,7 +5,7 @@ INSERT INTO users(username, passwordHash) VALUES ('nathan', '$2y$10$SvwZrUoVtZKD
 INSERT INTO users(username, passwordHash) VALUES ('mikaela', '$2y$10$SvwZrUoVtZKD3r6SGnJUjOq0oY9t9a5ca91Tf/EPp/62Bq7L3H.dG');
 INSERT INTO users(username, passwordHash) VALUES ('ryan', '$2y$10$SvwZrUoVtZKD3r6SGnJUjOq0oY9t9a5ca91Tf/EPp/62Bq7L3H.dG');
 
-INSERT INTO books(ISBN, title, author, description, coverImage) 
+INSERT INTO books(ISBN, title, author, year, coverImage) 
 VALUES 
 ('1111111111111', 'How to Impress your Professor', 'Student Etudiant', '2005', 'images/bookNoCover.png'),
 ('2222222222222', 'Learning WebDev in 24 Hours', 'Grey Squirrel', '1045', 'images/bookNoCover.png'),
