f11cf12f73cfd0206a2921d07a43dd250d23d85c
mcotter001 <cott0075@algonquinlive.com>
2025-03-29 21:40:45 -0400

    Finally fixed css and html for about us page

--- aboutUs.php
+++ aboutUs.php
@@ -19,34 +19,34 @@
             <div id="aboutUsContainer">
                 <div class="aboutUsItem">
                     <img class="aboutUsImage" src="images/mikaela-small.png" alt="A headshot of a young woman.">
-                    <div class="bookContents">
-                        <p class="bookTitle">Mikaela Cotter</p>
-                        <p class="bookDescription">Front End Designer</p><br>
-                        <p class="bookDescription">A big fan of rounded edges, pastel colors, and lower case letters.</p>
+                    <div class="aboutUsContents">
+                        <p class="aboutUsTitle">Mikaela Cotter</p>
+                        <p class="aboutUsDescription">Front End Designer</p><br>
+                        <p class="aboutUsDescription">A big fan of rounded edges, pastel colors, and lower case letters.</p>
                     </div>
                 </div>
                 <div class="aboutUsItem">
                     <img class="aboutUsImage" src="images/latk0004-headshot-small.jpg" alt="A headshot of a young man.">
-                    <div class="bookContents">
-                        <p class="bookTitle">Jonathan Latkowcer</p>
-                        <p class="bookDescription">Team Leader</p><br>
-                        <p class="bookDescription">Those who do can, build.<br><br>Those who can't, lead.</p>
+                    <div class="aboutUsContents">
+                        <p class="aboutUsTitle">Jonathan Latkowcer</p>
+                        <p class="aboutUsDescription">Team Leader</p><br>
+                        <p class="aboutUsDescription">Those who do can, build.<br><br>Those who can't, lead.</p>
                     </div>
                 </div>
                 <div class="aboutUsItem">
                     <img class="aboutUsImage" src="images/Ryan-AI.jpg" alt="A caricature of a young man with technology-related cartoon images around him.">
-                    <div class="bookContents">
-                        <p class="bookTitle">Ryan Marshall</p>
-                        <p class="bookDescription">Back End Developer</p><br>
-                        <p class="bookDescription">A database is a scary bottomless pit, and Ryan is our fearless spelunker.</p>
+                    <div class="aboutUsContents">
+                        <p class="aboutUsTitle">Ryan Marshall</p>
+                        <p class="aboutUsDescription">Back End Developer</p><br>
+                        <p class="aboutUsDescription">A database is a scary bottomless pit, and Ryan is our fearless spelunker.</p>
                     </div>
                 </div>
                 <div class="aboutUsItem">
                     <img class="aboutUsImage" src="images/nathan-small.png" alt="A headshot of a young man while he works on a computer.">
-                    <div class="bookContents">
-                        <p class="bookTitle">Nathan Tam</p>
-                        <p class="bookDescription">Front End Developer</p><br>
-                        <p class="bookDescription">Nathan made the mistake of showing us his excellent personal websites, so he was given the most complicated job.</p>
+                    <div class="aboutUsContents">
+                        <p class="aboutUsTitle">Nathan Tam</p>
+                        <p class="aboutUsDescription">Front End Developer</p><br>
+                        <p class="aboutUsDescription">Nathan made the mistake of showing us his excellent personal websites, so he was given the most complicated job.</p>
                     </div>
                 </div>
             </div>
